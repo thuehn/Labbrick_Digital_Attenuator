49a985f65965b082893dedfb49268b21908cb53e
Stefan Venz <stefan.venz@protonmail.com>
2015-10-20 15:52:31 +0200

    add new functions to control.h
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- src/control.h
+++ src/control.h
@@ -8,8 +8,11 @@ char errmsg[64];
 void get_serial_and_name(int device_count, char *device_name);
 char * get_device_data(unsigned int current_devices);
 int set_ramp(int id);
-int set_attenuation(unsigned int id);
-int set_triangle(unsigned int id);
+int set_ramp(int id, struct user_data *ud);
+int set_attenuation(int id);
+int set_attenuation(int id,struct user_data *ud);
+int set_triangle(int id);
+int set_triangle(int id, struct user_data *ud);
 void print_dev_info(int id);
 
 #endif
